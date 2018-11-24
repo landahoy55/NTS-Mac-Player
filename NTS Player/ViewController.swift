b5f3f71093fc1b22eeb0b781d409d922d30add4d
Pete <landahoy55@gmail.com>
2018-11-24 22:12:26 +0000

    Initial Commit

---
+++ NTS Player/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NTS Player
+//
+//  Created by P Malone on 24/11/2018.
+//  Copyright © 2018 landahoy55. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
